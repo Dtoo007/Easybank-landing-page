1f3e94485059d0ef612c2a386342aa5d829506c9
dtoo <taiwoabbas007@gmail.com>
2024-05-01 16:55:40 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
 import { useState } from 'react'
-import imageConfetti from './assets/imageConfetti.svg'
-import imageCurrency from './assets/imageCurrency.svg'
-import imagePlane from './assets/imagePlane.svg'
+import imageConfetti from './assets/imageConfetti.jpg'
+import imageCurrency from './assets/imageCurrency.jpg'
+import imagePlane from './assets/imagePlane.jpg'
 import iconApi from './assets/iconApi.svg'
 import iconBudgeting from './assets/iconBudgeting.svg'
 import iconOnline from './assets/iconOnline.svg'
 import iconOnboarding from './assets/iconOnboarding.svg'
-import viteLogo from '/vite.svg'
+import iconClose from './assets/iconClose.svg'
+import iconOpen from './assets/iconOpen.svg'
+import bgDesktop from './assets/bgDesktop.svg'
+import logo from './assets/logo.svg'
 import './App.css'
 
 
-const hero = {
-  header: 'Next generation digital banking',
-  sub: 'Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more'
-}
+const heros = [
+  {title: 'Next generation digital banking'},
+ { paragraph: 'Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more'}
+]
 
-const faetures = {
+const featre = {
   header: 'Why choose Easybank?',
   paragraph: 'We leverage open banking to turn your bank account into your financial hub. Control your finances like never before.'
 }
 
-const faetures = [
+const features = [
   {
     id: 1,
     icon: iconOnline,
@@ -60,7 +63,7 @@ const blog = [
   },
   {
     id: 1,
-    image: imageRestaurant,
+    // image: imageRestaurant,
     title: 'Treat yourself without worrying about money',
     author: 'Wilson Hutton',
     article: 'Our simple budgeting feature allows you to separate out your spending and set realistic limits each months. That means you ...'
@@ -81,12 +84,94 @@ const blog = [
   },
 ]
 
+const navs = [
+  { id: 1, label: 'Home' },
+  { id: 2, label: 'About' },
+  { id: 3, label: 'Contact' },
+  { id: 3, label: 'Blog' },
+  { id: 3, label: 'Careers' }
+];
 
 
 export default function App () {
   return(
     <div>
-
+      <Nav navs={navs}/>
+      <Hero/>
+      <Section features={features}/>
     </div>
+  )
+}
+
+function Button ({children}) {
+  return (
+    <button>{children}</button>
+  )
+}
+
+function Nav ({navs}) {
+  const [navToggle, setNavToggle] = useState(true);
+  function handleToggle () {
+    setNavToggle(prev => !prev)
+  }
+return (
+  <nav>
+    <div className='nav'>
+    <diV><img src={logo} alt='logo'></img></diV>
+    <div className='nav-bg'>
+    <div className={navToggle ? 'nav-links' : 'nav-links-mobile'}> 
+          { navs.map(nav => (
+      <ul key={nav.id}>
+        <li>{nav.label}</li>
+      </ul>
+    ))}
+    </div>
+    </div>
+
+    <div><Button>Request Invite</Button></div>
+    <div className='hamburger'>
+    { navToggle ? <img src={iconOpen} alt='hamburger-open' className='hamburger-open' onClick={handleToggle}></img> :
+      <img src={iconClose} alt='hamburger-close' className='hamburger-close' onClick={handleToggle}></img>
+      }
+    </div>
+  </div>
+  </nav>
+)
+}
+
+
+function Hero () {
+  return(
+    <header>
+      <div className='body'>
+        <div  className='content'>
+        <h1>Next generation digital banking</h1>
+        <p>Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more</p>
+        <div><Button>Request invite</Button></div>
+        </div>
+         <div className='hero-img'>
+       <img src={bgDesktop} alt='image mockup' className='bg-desktop'></img></div>
+      </div>
+    </header>
+  )
+}
+
+function Section ({features}) {
+  return (
+    <section>
+      <div>
+        <h1>Why choose Easybank</h1>
+        <p>We leverage open banking to turn your bank account into your financial hub. Control your finances like never before.</p>
+      </div>
+      <div>
+        {features.map(feature => (
+          <div key={feature.id}>
+          <img>{feature.icon}</img>
+          <h4>{feature.heading}</h4>
+          <p>{feature.body}</p>
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
